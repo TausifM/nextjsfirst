0ab0f8585c096dcfd7575fe70f604c5a731bab95
TausifM <heytausifsheikh@gmail.com>
2023-05-03 18:06:47 +0530

    padding hover set

--- components/Header/DropDownSubMenu.jsx
+++ components/Header/DropDownSubMenu.jsx
@@ -7,13 +7,13 @@ const DropDownSubMenu = ({ title, subMenu, link }) => {
         return setSubMenuActive(!subMenuActive);
     }
     return (
-        <li className="dropdown-submenu" style={{ fontSize: "1.111rem", fontFamily: 'Manrope'}}>
+        <li className="dropdown-submenu ps-2 pe-2 pt-3 pb-3" style={{ fontSize: "1.111rem", fontFamily: 'Manrope'}}>
             <Link className="dropdown-item text-white" href={link} onClick={handleToggle} >
                 {title} &raquo;
             </Link>
             <ul className={`dropdown-menu  ${subMenuActive ? 'd-block' : ''}`}>
                 {subMenu !== undefined ? subMenu.map((item, index) => (
-                    <li className="dropdown-submenu" key={index}>
+                    <li className="dropdown-submenu ps-2 pe-2 pt-3 pb-3" key={index}>
                         <Link className="dropdown-item text-white" href={item.link} 
                         style={{ fontSize: "1.111rem" }}
                         >
